432b964edae5479f750b4ba24fcb427153cc3ca1
Nick Steinly <njs40@duke.edu>
2023-05-26 02:31:09 -0400

    readded the 'all fields required' errror checking to the backend

--- frontend/src/javascript/components/login.component.js
+++ frontend/src/javascript/components/login.component.js
@@ -44,54 +44,54 @@ const Login = () => {
 
   return (
     <div>
-    {alert && <Alert message={alert.message} type={alert.type} />}
-    <form onSubmit={handleSubmit}>
-      <h3>Sign In</h3>
+      {alert && <Alert message={alert.message} type={alert.type} />}
+      <form onSubmit={handleSubmit}>
+        <h3>Sign In</h3>
 
-      <div className="mb-3">
-        <label>Username</label>
-        <input
-          type="username"
-          className="form-control"
-          placeholder="Enter username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
-
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
+        <div className="mb-3">
+          <label>Username</label>
           <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
+            type="username"
+            className="form-control"
+            placeholder="Enter username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
           />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
         </div>
-      </div>
 
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
-      </p>
-    </form>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
     </div>
   );
 };
--- frontend/src/javascript/components/signup.component.js
+++ frontend/src/javascript/components/signup.component.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 
+const Alert = ({ message, type }) => {
+  return <div className={`alert alert-${type}`}>{message}</div>;
+};
+
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [alert, setAlert] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevents default form submission behavior
@@ -25,74 +30,82 @@ const SignUp = () => {
       },
       body: JSON.stringify(data),
     })
-      .then(response => response.json())
-      .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         // Handle the response from the backend
-        console.log(result);
+        if (result.error) {
+          setAlert({ type: 'danger', message: result.error });
+        } else {
+          setAlert({ type: 'success', message: result.message });
+          // Perform any additional actions on success
+        }
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle any errors that occurred during the request
         console.error('Error:', error);
       });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Sign Up</h3>
+    <div>
+      {alert && <Alert message={alert.message} type={alert.type} />}
+      <form onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
 
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
 
-      <div className="mb-3">
-        <label>Username</label>
-        <input
-          type="username"
-          className="form-control"
-          placeholder="Enter username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
+        <div className="mb-3">
+          <label>Username</label>
+          <input
+            type="username"
+            className="form-control"
+            placeholder="Enter username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
 
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
 
-      <div className="mb-3">
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Confirm password"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        />
-      </div>
+        <div className="mb-3">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+        </div>
 
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Sign Up
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Already registered <a href="/sign-in">sign in?</a>
-      </p>
-    </form>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    </div>
   );
 };
 
