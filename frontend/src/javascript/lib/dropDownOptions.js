acf88416b96c4db8d1e8cfff4491210ab1c876b5
Nick Steinly <njs40@duke.edu>
2023-06-26 20:23:54 -0400

    added logout to sidenav

---
+++ frontend/src/javascript/lib/dropDownOptions.js
@@ -0,0 +1,26 @@
+export const chartTimeOption = [
+    {
+        id: 0,
+        icon: <DashboardIcon/>,
+        text: "Dashboard",
+        link: "/duke-net-nutrition/dashboard"
+    },
+    {
+        id: 1,
+        icon: <MealIcon/>,
+        text: "Create Meal",
+        link: "/duke-net-nutrition/create-meal"
+    },
+    {
+        id: 2,
+        icon: <SurveyIcon/>,
+        text: "User Preferences",
+        link: "/duke-net-nutrition/user-preferences"
+    },
+    {
+        id: 3,
+        icon: <ProfileIcon/>,
+        text: "Profile",
+        link: "/duke-net-nutrition/profile"
+    }
+]
