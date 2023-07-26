2933781c8b4922e666beca823f8cc511df852af3
Nick Steinly <njs40@duke.edu>
2023-07-26 01:28:18 -0400

    fixed the centering of the pages with the dynamic size of the sidenav in a non jank way using the fact that css variables are set once when the css file is parsed so javascript must be used to reassign css variables once the css file has already been parsed/loaded

--- view/src/javascript/components/Sidenav.js
+++ view/src/javascript/components/Sidenav.js
@@ -11,23 +11,34 @@ import { useAuth } from "../context/AuthProvider";
 export default function Sidenav() {
     const [open, setopen] = useState(true)
     const toggleOpen = () => {
+
+        /*CSS variables are meant to be set during the parsing of CSS and provide a way to define reusable values that can be used throughout your stylesheets. Once a CSS variable is set, it will maintain its value for the entire duration of the page load.
+
+        If you want to change the value of a CSS variable dynamically, you need to use JavaScript, as shown in the previous example. JavaScript allows you to interact with the DOM and update CSS variables at runtime, providing a way to achieve dynamic theming or adjust styles based on user interactions or other conditions.*/
+
+        const root = document.documentElement;
+        if(open){
+            root.style.setProperty('--sidenav-column-width', '60px');
+        } else {
+            root.style.setProperty('--sidenav-column-width', '200px');
+        }
         setopen(!open)
     }
     const { logout } = useAuth();
   return (
-    <div class={open ? 'sidenav' : 'sidenavClosed'}>
-        <button class={'menuBtn'} onClick={toggleOpen}>
-            {open? <KeyboardDoubleArrowLeftIcon />: <KeyboardDoubleArrowRightIcon />}
+    <div className={open ? 'sidenav' : 'sidenavClosed'}>
+        <button className={'menuBtn'} onClick={toggleOpen}>
+            {open ? <KeyboardDoubleArrowLeftIcon /> : <KeyboardDoubleArrowRightIcon />}
         </button>
         {navData.map(item =>{
             return <NavLink key={item.id} id={'sideitem'} to={item.link}>
             {item.icon}
-            <span class={open ? 'linkTextVisible' : 'linkTextHidden'}>{item.text}</span>
+            <span className={open ? 'linkTextVisible' : 'linkTextHidden'}>{item.text}</span>
         </NavLink>
         })}
-        <div class={'logoutButtonContainer'}>
-            <button class={'logoutButton'} onClick={logout}>
-                <LogoutIcon/> <span class={'linkTextVisible'}>{open ? 'Logout' : null}</span>
+        <div className={'logoutButtonContainer'}>
+            <button className={'logoutButton'} onClick={logout}>
+                <LogoutIcon/> <span className={'linkTextVisible'}>{open ? 'Logout' : null}</span>
                 <br/>
                 <br/>
             </button>
